4dfde7ccd89188dfaa8339acd34427e631a292d2
Jørn Salvesen <jornsalve@gmail.com>
2023-03-05 08:51:01 +0100

    Add HTTPError exception handling

--- task3.py
+++ task3.py
@@ -1,9 +1,14 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-# Open and parse the Wikipedia page
-html = urlopen('https://en.wikipedia.org/wiki/Web_scraping')
-bs = BeautifulSoup(html, 'html.parser')
+# Open and parse the Wikipedia page, with exception handling in case the page is down or doesn't exist
+try:
+    html = urlopen('https://en.wikipedia.org/wiki/Web_scraping')
+except HTTPError as e:
+    print(e)
+else:
+    bs = BeautifulSoup(html, 'html.parser')
 
 # Find links in "See also" section and print the first paragraph of each
 for link in bs.find('div', {'class': 'div-col'}).ul.find_all('a'):
